0d576970b764395e526d6fce018b1ce7ffc07d2e
Yovinne <yovinne.shadora@students.jkuat.ac.ke>
2025-04-16 22:21:01 +0300

    Initial commit for emotion detection app

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+import cv2
+import os
+
+app = Flask(__name__)
+model = load_model('emotion_detection_model.h5')
+emotion_labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'image' not in request.files:
+        return 'No image uploaded', 400
+
+    file = request.files['image']
+    img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (48, 48))
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0) / 255.0
+
+    prediction = model.predict(img)
+    emotion = emotion_labels[np.argmax(prediction)]
+
+    return render_template('index.html', prediction=emotion)
+
+if __name__ == '__main__':
+    app.run(debug=True)
